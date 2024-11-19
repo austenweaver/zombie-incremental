906dc1a2216dc3e3d7d6da6b4226bd911d400d3a
Austen Weaver <austenweaver@protonmail.com>
2024-11-19 14:37:55 -0500

    Refactor mapGraph test for improved readability and formatting

--- resources/scripts/__tests__/mapGraph.test.js
+++ resources/scripts/__tests__/mapGraph.test.js
@@ -22,15 +22,15 @@ describe('MapGraph', () => {
 		const room1 = new Room('Room1', [['empty']]);
 		const room2 = new Room('Room2', [['empty']]);
 		const room3 = new Room('Room3', [['empty']]);
-
+	
 		const graph = new MapGraph();
 		graph.addRoom('Room1', room1);
 		graph.addRoom('Room2', room2);
 		graph.addRoom('Room3', room3);
 		graph.connectRooms('Room1', 'Room2', 10);
 		graph.connectRooms('Room2', 'Room3', 20);
-
+	
 		const path = graph.findPath('Room1', 'Room3');
 		expect(path).toEqual(['Room1', 'Room2', 'Room3']);
-	});
+	});	
 });
