b1265c3280fc698003e6d55bb5bf89e0f08d282e
Austen Weaver <austenweaver@protonmail.com>
2024-11-26 19:43:37 -0500

    Update cell dimensions in render and CSS for responsive design

--- resources/scripts/render.js
+++ resources/scripts/render.js
@@ -21,11 +21,12 @@ export function renderMap(mapData) {
 				const cellDiv = document.createElement('div');
 				let cellType = typeof cell === 'string' ? cell : cell.type;
 				cellDiv.className = `cell ${cellType.toLowerCase()}`; // Ensure class is in lowercase
-				cellDiv.style.width = '50px'; // Set a fixed width for cells
-				cellDiv.style.height = '50px'; // Set a fixed height for cells
+				cellDiv.style.width = '1.5vw'; // Set a fixed width for cells
+				cellDiv.style.height = '1.5vw'; // Set a fixed height for cells
 				cellDiv.style.display = 'flex'; // Align content to center
 				cellDiv.style.alignItems = 'center'; // Align content to center
 				cellDiv.style.justifyContent = 'center'; // Align content to center
+				console.log(cellType.toLowerCase())
 				if (cellType.toLowerCase() === 'wall' || cellType.toLowerCase() === 'door' || cellType.toLowerCase() === 'portal') {
 					cellDiv.textContent = 'X'; // Add 'X' to unwalkable cells
 				}
